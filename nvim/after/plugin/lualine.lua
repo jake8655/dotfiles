7d4fb1b29ca42e873418b497e27f1e159c7ba476
Jake <dominik8655@gmail.com>
2024-04-30 19:48:07 +0200

    feat(nvim): show relative filepath in status

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,5 +3,12 @@ require('lualine').setup {
     lualine_x = { 'fileformat', 'filetype' },
     lualine_y = { 'progress', 'location' },
     lualine_z = { "os.date('%H:%m:%S')" },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1,
+      },
+    },
   },
 }
