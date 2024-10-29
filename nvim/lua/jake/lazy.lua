8f881663fbf500a1e0400c44d3c0f65e3097e9a3
Jake <dominik8655@gmail.com>
2024-10-29 18:52:22 +0100

    feat(nvim): add git status for oil.nvim

--- nvim/lua/jake/lazy.lua
+++ nvim/lua/jake/lazy.lua
@@ -243,11 +243,25 @@ require('lazy').setup({
         'size',
         'mtime',
       },
+      win_options = {
+        signcolumn = 'yes',
+      },
       watch_for_changes = true,
       view_options = {
         show_hidden = true,
       },
     },
+  },
+
+  -- Git status for oil.nvim
+  {
+    'refractalize/oil-git-status.nvim',
+    dependencies = {
+      'stevearc/oil.nvim',
+    },
+    config = function()
+      require('oil-git-status').setup { show_ignored = false }
+    end,
   },
 
   -- require 'kickstart.plugins.autoformat',
