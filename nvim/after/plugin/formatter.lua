3b890c5d39a0a5df5dfe4be32fc728541bd156de
Jake <dominik8655@gmail.com>
2024-12-20 23:11:03 +0100

    feat(nvim): recognize `bun.lock` as json file (oven-sh/bun#15705)

--- nvim/after/plugin/formatter.lua
+++ nvim/after/plugin/formatter.lua
@@ -158,3 +158,12 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     vim.cmd 'FormatWrite'
   end,
 })
+
+local bun_group = vim.api.nvim_create_augroup('bun', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = 'bun.lock',
+  group = bun_group,
+  callback = function()
+    vim.bo.filetype = 'jsonc'
+  end,
+})
