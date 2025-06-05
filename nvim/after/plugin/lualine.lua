22e04f8f490193e6371bb0ced44217d7b7edcd68
Jake <dominik8655@gmail.com>
2025-06-05 16:19:02 +0200

    fix(neovim): lualine time

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -2,7 +2,7 @@ require('lualine').setup {
   sections = {
     lualine_x = { 'fileformat', 'filetype' },
     lualine_y = { 'progress', 'location' },
-    lualine_z = { "os.date('%H:%m:%S')" },
+    lualine_z = { "os.date('%H:%M:%S')" },
     lualine_c = {
       {
         'filename',
