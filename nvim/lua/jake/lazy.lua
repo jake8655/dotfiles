f73f5bb394f28baff5ee22b795f4cbe83414897e
Jake <dominik8655@gmail.com>
2025-08-11 18:32:27 +0200

    feat(nvim): add nicer markdown rendering

--- nvim/lua/jake/lazy.lua
+++ nvim/lua/jake/lazy.lua
@@ -447,6 +447,12 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    opts = {},
+  },
+
   -- require 'kickstart.plugins.autoformat',
   ---@diagnostic disable-next-line: missing-fields
 }, {
