fb638e98a6757dc8cc863da4137818a6417ee83d
Jake <dominik8655@gmail.com>
2025-09-06 23:34:42 +0200

    chore(git): format git log and make it pretty

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -24,6 +24,7 @@ alias bm="bluetoothctl connect 94:F6:D6:B6:6B:42"
 alias dbm="bluetoothctl disconnect"
 alias wallp="wallpaper-cava --config /home/jake/.dotfiles/cava/config.toml"
 alias gdvim="nvim --listen ~/.cache/nvim/godot.pipe"
+alias glg="g lg"
 
 # Browse projects
 function pj() {
