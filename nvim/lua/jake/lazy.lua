98609730563438a82c4ed6cb78213d8cf7f7183b
Jake <dominik8655@gmail.com>
2026-07-09 18:04:11 +0200

    chore(nvim): remove copilot

--- nvim/lua/jake/lazy.lua
+++ nvim/lua/jake/lazy.lua
@@ -81,17 +81,17 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'github/copilot.vim',
-    config = function()
-      vim.keymap.set('i', '<C-l>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-      })
-      vim.g.copilot_no_tab_map = true
-      vim.cmd [[Copilot disable]]
-    end,
-  },
+  -- {
+  --   'github/copilot.vim',
+  --   config = function()
+  --     vim.keymap.set('i', '<C-l>', 'copilot#Accept("\\<CR>")', {
+  --       expr = true,
+  --       replace_keycodes = false,
+  --     })
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.cmd [[Copilot disable]]
+  --   end,
+  -- },
 
   -- Statusline
   {
